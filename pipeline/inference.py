8f8f12ed2c61ea07991fa309f1c39036b410b757
katerinamerkulova <59052477+katerinamerkulova@users.noreply.github.com>
2023-05-30 14:42:37 +0300

    refactor

--- pipeline/inference.py
+++ pipeline/inference.py
@@ -1,42 +1,35 @@
-from joblib import load
-
 import pandas as pd
 from catboost import CatBoostClassifier
 from transliterate import translit
 
 from features import feats, categorical_feats, characteristic_feats
-from utils import postprocess
 
 
-pd.set_option('use_inf_as_na', True)
+pd.set_option("use_inf_as_na", True)
 GLOBAL_SEED = 42
-BY_CATEGORY = True
+
 test_features = pd.read_parquet("../../data/test_processed_chars_by_cat_color.parquet")
 test_features.fillna(0, inplace=True)
 
-feats = feats + characteristic_feats + categorical_feats + ['variantid1', 'variantid2']
+feats = feats + characteristic_feats + categorical_feats + ["variantid1", "variantid2"]
 # feats = feats + characteristic_feats
-model_name = 'CatBoost_chars_by_cat_color_catboost_by_cat'
+model_name = "CatBoost_chars_by_cat_color_catboost_by_cat"
 
 new_model = CatBoostClassifier()
 submission_example = test_features.copy()
 
-if BY_CATEGORY:
-    groups = submission_example['cat3_grouped1']
-    for group in set(groups):
-        group_name = '_'.join(group.replace('/', ' ').split())
-        model_path = translit(f"../models/categories/{model_name}_{group_name}", 'ru', reversed=True)
-        model = new_model.load_model(model_path + '.cbm', format='cbm')
-        X = submission_example.loc[submission_example['cat3_grouped1'] == group]
-        submission_example.loc[X.index, 'target'] = model.predict_proba(X[feats])[:, 1]
-else:
-    model = new_model.load_model(f'../models/{model_name}.cbm', format='cbm')
-    # model = load(f'../models/{model_name}.joblib')
-    submission_example["target"] = model.predict_proba(test_features[feats])[:, 1]
-    submission_example["target"] = submission_example.apply(lambda x: postprocess(x['target'], x['cat3_grouped1']), axis=1)
+
+groups = submission_example["cat3_grouped1"]
+for group in set(groups):
+    group_name = "_".join(group.replace("/", " ").split())
+    model_path = translit(f"../models/categories/{model_name}_{group_name}", "ru", reversed=True)
+    model = new_model.load_model(model_path + ".cbm", format="cbm")
+    X = submission_example.loc[submission_example["cat3_grouped1"] == group]
+    submission_example.loc[X.index, "target"] = model.predict_proba(X[feats])[:, 1]
+
 
 submission_example = submission_example[["variantid1", "variantid2", "target", "cat3_grouped1"]]
-submission_example.drop_duplicates(subset=['variantid1', 'variantid2']).merge(
+submission_example.drop_duplicates(subset=["variantid1", "variantid2"]).merge(
     test_features[["variantid1", "variantid2"]].drop_duplicates(["variantid1", "variantid2"]),
     on=["variantid1", "variantid2"]
 ).to_csv(f"../submission_files/submission_{model_name}.csv", index=False)
--- pipeline/minilm_finetuning.py
+++ pipeline/minilm_finetuning.py
@@ -1,5 +1,5 @@
 """
-This example loads the pre-trained SentenceTransformer model 'nli-distilroberta-base-v2' from the server.
+This example loads the pre-trained SentenceTransformer model "nli-distilroberta-base-v2" from the server.
 It then fine-tunes this model for some epochs on the STS benchmark dataset.
 
 Note: In this example, you must specify a SentenceTransformer model.
@@ -18,12 +18,12 @@ from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader
 
 
-model_path = Path.cwd() / 'models'
-model_name = 'all-MiniLM-L6-v2'
+model_path = Path.cwd() / "models"
+model_name = "all-MiniLM-L6-v2"
 
 #### Just some code to print debug information to stdout
-logging.basicConfig(format='%(asctime)s - %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S',
+logging.basicConfig(format="%(asctime)s - %(message)s",
+                    datefmt="%Y-%m-%d %H:%M:%S",
                     level=logging.INFO,
                     handlers=[LoggingHandler()])
 #### /print debug information to stdout
@@ -32,7 +32,7 @@ logging.basicConfig(format='%(asctime)s - %(message)s',
 # Read the dataset
 train_batch_size = 8
 num_epochs = 100
-model_save_path = '../chars_names_minilm/' + model_name + '-' + datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+model_save_path = "../chars_names_minilm/" + model_name + "-" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
 # Load a pre-trained sentence transformer model
 model = SentenceTransformer(model_name, device=1)
@@ -43,27 +43,27 @@ logging.info("Read STSbenchmark train dataset")
 dataset = pd.read_parquet("../../data/hackathon_files_for_participants_ozon/train_pairs.parquet")
 etl = pd.read_parquet("../../data/hackathon_files_for_participants_ozon/train_data.parquet")
 
-mode = 'FULL' # CHAR, NAME
+mode = "FULL" # CHAR, NAME
 
 samples = []
 for i, row in dataset.iterrows():
-    chars1 = etl.loc[etl['variantid'] == row['variantid1'], 'characteristic_attributes_mapping'].item()
-    chars2 = etl.loc[etl['variantid'] == row['variantid1'], 'characteristic_attributes_mapping'].item()
-    char1 = ' '.join(sorted([f'[CHAR]{char}[VAL]{val}' for char, val in json.loads(chars1).items()])) if chars1 else ''
-    char2 = ' '.join(sorted([f'[CHAR]{char}[VAL]{val}' for char, val in json.loads(chars2).items()])) if chars2 else ''
-    name1 = etl.loc[etl['variantid'] == row['variantid1'], 'name'].item()
-    name2 = etl.loc[etl['variantid'] == row['variantid2'], 'name'].item()
+    chars1 = etl.loc[etl["variantid"] == row["variantid1"], "characteristic_attributes_mapping"].item()
+    chars2 = etl.loc[etl["variantid"] == row["variantid1"], "characteristic_attributes_mapping"].item()
+    char1 = " ".join(sorted([f"[CHAR]{char}[VAL]{val}" for char, val in json.loads(chars1).items()])) if chars1 else ""
+    char2 = " ".join(sorted([f"[CHAR]{char}[VAL]{val}" for char, val in json.loads(chars2).items()])) if chars2 else ""
+    name1 = etl.loc[etl["variantid"] == row["variantid1"], "name"].item()
+    name2 = etl.loc[etl["variantid"] == row["variantid2"], "name"].item()
 
-    if mode == 'NAME':
+    if mode == "NAME":
         text1, text2 = name1, name2
-    if mode == 'CHAR':
+    if mode == "CHAR":
         if not chars1 or not chars2:
             continue
         text1, text2 = char1, char2
-    if mode == 'FULL':
-        text1, text2 = f'{name1} {char1}', f'{name2} {char2}'
+    if mode == "FULL":
+        text1, text2 = f"{name1} {char1}", f"{name2} {char2}"
 
-    inp_example = InputExample(texts=[text1, text2], label=float(row['target']))
+    inp_example = InputExample(texts=[text1, text2], label=float(row["target"]))
     samples.append(inp_example)
 
 train_samples, test_samples = train_test_split(samples, test_size=0.3)
@@ -76,7 +76,7 @@ train_loss = losses.CosineSimilarityLoss(model=model)
 
 # Development set: Measure correlation between cosine score and gold labels
 logging.info("Read STSbenchmark dev dataset")
-evaluator = EmbeddingSimilarityEvaluator.from_input_examples(dev_samples, name='sts-dev')
+evaluator = EmbeddingSimilarityEvaluator.from_input_examples(dev_samples, name="sts-dev")
 
 
 # Configure the training. We skip evaluation in this example
@@ -100,5 +100,5 @@ model.fit(train_objectives=[(train_dataloader, train_loss)],
 ##############################################################################
 
 model = SentenceTransformer(model_save_path)
-test_evaluator = EmbeddingSimilarityEvaluator.from_input_examples(test_samples, name='sts-test')
+test_evaluator = EmbeddingSimilarityEvaluator.from_input_examples(test_samples, name="sts-test")
 test_evaluator(model, output_path=model_save_path)
